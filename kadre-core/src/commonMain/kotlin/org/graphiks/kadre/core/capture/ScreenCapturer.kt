5904b8b4912ec21f74bcec38fca577383ebd8b1c
Alexandre Mommers <alexandre.mommers@gmail.com>
2026-06-06 16:13:02 +0200

    feat(capture): add ScreenCapturer interface and CaptureSession abstract class

---
+++ kadre-core/src/commonMain/kotlin/org/graphiks/kadre/core/capture/ScreenCapturer.kt
@@ -0,0 +1,9 @@
+package org.graphiks.kadre.core.capture
+
+interface ScreenCapturer {
+    suspend fun enumerateDisplays(): List<DisplayInfo>
+    suspend fun enumerateWindows(): List<WindowInfo>
+    suspend fun createSession(source: CaptureSource, config: CaptureConfig = CaptureConfig()): CaptureSession
+    suspend fun requestPermission(): CapturePermission
+    fun permissionStatus(): CapturePermission
+}
