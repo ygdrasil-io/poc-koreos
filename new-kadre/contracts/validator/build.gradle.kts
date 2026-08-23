602c2d37d7e8a77dee4365ca554e32e3c67dbad1
ygdrasil-io <alexandre.mommers@gmail.com>
2026-08-23 22:15:40 +0200

    test: add contract registry validator

---
+++ new-kadre/contracts/validator/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("org.jetbrains.kotlin.multiplatform")
+}
+
+kotlin {
+    jvmToolchain(25)
+    jvm()
+
+    sourceSets {
+        jvmTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+}
