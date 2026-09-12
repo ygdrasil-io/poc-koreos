e95917f181269a52607de86715a3dee4e5eef62b
ygdrasil-io <alexandre.mommers@gmail.com>
2026-09-12 16:38:33 +0200

    feat(runtime): bound capture frame delivery

--- kadre/runtime/src/commonMain/kotlin/org/graphiks/kadre/internal/runtime/SessionRuntime.kt
+++ kadre/runtime/src/commonMain/kotlin/org/graphiks/kadre/internal/runtime/SessionRuntime.kt
@@ -138,7 +138,9 @@ internal class SessionRuntime(
         RuntimeCaptureManager(
             port = port,
             maxConcurrentSessions = policy.resources.maxConcurrentCaptureSessions,
+            capturePolicy = policy.capture,
             eventStampSource = ::nextStamp,
+            onCaptureFailure = ::eventDeliveryFailed,
         )
     }
     private val runtimeCapture: CaptureManager = runtimeCaptureManager ?: UnsupportedCaptureManager()
